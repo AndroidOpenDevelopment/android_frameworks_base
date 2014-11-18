2b4bb179c6435c1f3c0843353a4afe263459f7a8
Matt Garnes <matt@cyngn.com>
2014-11-17 16:56:16 -0800

    status bar: battery level text
    1. Add TextView to display battery level percentage next to the battery icon.
    2. Remove support for displaying battery percentage on the actual battery icon itself.
    3. Also respect view on lockscreen and opened notification drawer.
    
    Change-Id: Ie816caac3666587c69e4e3e4fb6da1e7e899b48c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2717,6 +2717,14 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+
+         * Status bar show battery percent
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
+
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
